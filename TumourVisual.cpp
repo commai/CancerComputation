93cc914b014053864d46060eb064212efa6bba7f
Vardan Sawhney <vsawhney@uwaterloo.ca>
2019-11-21 13:27:42 -0500

    update

--- TumourVisual.cpp
+++ TumourVisual.cpp
@@ -1,4 +1,4 @@
-#include "vtkCubSource.h"
+#include "vtkCubeSource.h"
 #include "vtkPolyDataMapper.h"
 #include "vtkRenderer.h"
 #include "vtkRenderWindow.h"
@@ -6,20 +6,81 @@
 #include "vtkInteractorStyleTrackballCamera.h"
 #include "vtkPolyData.h"
 #include "vtkGlyph3DMapper.h"
-
+#include "vtkPointData.h"
+#include "vtkFloatArray.h"
+#include "vtkLookupTable.h"
+ 
 vtkSmartPointer<vtkRenderer> renderer;
 vtkSmartPointer<vtkRenderWindow> renderWindow;
-vtkSmartPointer<vtkRenderWindowInteractor> RenderWindowInteractor;
+vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor;
+ 
 vtkSmartPointer<vtkPoints> points;
+ 
+vtkSmartPointer<vtkFloatArray> scalar;
 
 // Init the renderer and rendering window w interactor object
-void intializeRendering(){
-	renderer = vtkSmartPointer<vtkRenderer>::New() // creating new renderer
-	renderer->SetBackground(1.,1.,1.); //setting background to white
+void initializeRendering() {
+    renderer = vtkSmartPointer<vtkRenderer>::New(); //creating new renderer
+    renderer->SetBackground(1., 1., 1.); //setting background color to white
+ 
+    renderWindow = vtkSmartPointer<vtkRenderWindow>::New(); //creating renderer window
+    renderWindow->SetSize(1000, 1000); //setting size of the window
+    renderWindow->AddRenderer(renderer); //adding previously created renderer
+ 
+    renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New(); //creating render
+    renderWindowInteractor->SetRenderWindow(renderWindow); //adding previously created renderer window
+ 
+    points = vtkSmartPointer<vtkPoints>::New();         //vector with points
+    scalar = vtkSmartPointer<vtkFloatArray>::New(); //vector with scalar values of the points
+    scalar->SetNumberOfComponents(1); //one value per point
+}
 
-	renderWindow = vtkSmartPointer<vtkRenderWindow>::New()//creating renderer window
-	renderWindow->SetSize(1000,1000); //size 
-	renderWindow->AddRenderer(renderer);// adding previously created renderer
+//Assuming cell objects are stored in STL vector structure, and each cell 
+// contains information about (x,y,z) position in 3D space 
+// - addCells - add cells to the scene  
+void addCells(vector<Cell>::iterator begin, vector<Cell>::iterator end) {
+    vector<Cell>::iterator cell = begin;
+    for (cell; cell<end; cell++) {        
+     	if (cell->z < 250) {             
+     		points->InsertNextPoint(cell->x,cell->y,cell->z);
+        } else {
+            points->InsertNextPoint(cell->x,cell->y,cell->z+50);
+        }
+        scalar->InsertNextValue((double)cell->nSpots/26.);
+    }
+}
 
-	points = vtkSmartPointer<vtkPoints>::New(); //vector w points
+
+void visualizeLesion(vector<Cell>::iterator begin, vector<Cell>::iterator end) {
+    initializeRendering();
+    addCells(begin, end); 
+    //creating new color table
+    vtkSmartPointer<vtkLookupTable> colorLookupTable = vtkSmartPointer<vtkLookupTable>::New();
+    colorLookupTable->SetNumberOfColors(256);
+    colorLookupTable->SetHueRange( 0.667, 0.0);
+    colorLookupTable->Build();
+    // Combine into a polydata
+    vtkSmartPointer<vtkPolyData> polydata = vtkSmartPointer<vtkPolyData>::New();
+    polydata->SetPoints(points);
+    polydata->GetPointData()->SetScalars(scalar);
+    vtkSmartPointer<vtkCubeSource> cubeSource = vtkSmartPointer<vtkCubeSource>::New();
+    vtkSmartPointer<vtkGlyph3D> glyph3D = vtkSmartPointer<vtkGlyph3D>::New();
+    glyph3D->SetColorModeToColorByScalar();
+    glyph3D->SetSourceConnection(cubeSource->GetOutputPort());
+    glyph3D->ScalingOff();
+    glyph3D->SetInputData(polydata);
+    glyph3D->Update();
+    // Create a mapper and actor
+    vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    mapper->SetInputConnection(glyph3D->GetOutputPort());
+    mapper->SetLookupTable(colorLookupTable);
+    vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
+    actor->SetMapper(mapper);
+    // Add the actor to the scene
+    renderer->AddActor(actor);
+    // Render
+    renderWindow->Render();
+    vtkSmartPointer<vtkInteractorStyleTrackballCamera> style = vtkSmartPointer<vtkInteractorStyleTrackballCamera>::New(); //like paraview
+    renderWindowInteractor->SetInteractorStyle( style );
+    renderWindowInteractor->Start();
 }
